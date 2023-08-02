049e78f418b7061998b9dce1a208a6a747efd42b
psam <adedoyinsamuel510@gmail.com>
2023-08-02 23:32:19 +0100

    Initial commit

---
+++ blnr_linktree-master/tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  darkMode: 'class',
+  content: ["./src/**/*.{html,js}"
+
+    
+  ],
+
+
+  theme: {
+    extend: {},
+  },
+  plugins: [
+    require('preline/plugin'),
+  ],
+}
